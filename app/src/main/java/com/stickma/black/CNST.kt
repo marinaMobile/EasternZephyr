ff407a4df9226091dc065b80f0e7a8e8b79d4009
Marina <marina@Marinas-MacBook-Pro.local>
2022-08-11 14:08:07 +0300

    first commit

---
+++ app/src/main/java/com/stickma/black/CNST.kt
@@ -0,0 +1,18 @@
+package com.stickma.black
+
+object CNST {
+    const val AF_DEV_KEY = "wYfgASGWxksNSZvR6hGcqG"
+    const val jsoupCheck = "8j3g"
+    const val ONESIGNAL_APP_ID = "3a9009c1-6a28-48b3-b748-ce6c9b5c564d"
+
+    var lru = "http://easternzephyr.xyz/go.php?to=1&"
+
+
+    val odone = "sub_id_1="
+    val twoSub = "sub_id_2="
+
+    var MAIN_ID: String? = ""
+    var C1: String? = "c11"
+    var D1: String? = "d11"
+    var DEV: String = "false"
+}
---
+++ app/src/main/java/com/stickma/white/Game.kt
@@ -0,0 +1,12 @@
+package com.stickma.white
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.stickma.R
+
+class Game : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game)
+    }
+}
